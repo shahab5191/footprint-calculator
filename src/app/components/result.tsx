9071a72ae0678f41786c385b90bb0d8388d271df
shahab <oveysi.shahab@gmail.com>
2024-08-19 23:17:55 +0330

    fix: hiding result when name is empty

--- src/app/components/result.tsx
+++ src/app/components/result.tsx
@@ -11,14 +11,18 @@ const ResultSide = () => {
     return (
         <div className="col-span-4 bg-primary flex justify-center items-center">
             <div className="w-primary grid gap-2xl">
-                <h2 className="text-foreground text-md font-extralight w-full">
-                    Hi {username}
-                </h2>
-                <div className="flex flex-col gap-md">
-                    <ResultOutput label="annual" amount={annual} />
-                    <hr className="border-seperator w-primary" />
-                    <ResultOutput label="daily" amount={daily} />
-                </div>
+                {username === "" ? null : (
+                    <>
+                        <h2 className="text-foreground text-md font-extralight w-full">
+                            Hi {username}
+                        </h2>
+                        <div className="flex flex-col gap-md">
+                            <ResultOutput label="annual" amount={annual} />
+                            <hr className="border-seperator w-primary" />
+                            <ResultOutput label="daily" amount={daily} />
+                        </div>
+                    </>
+                )}
                 <div className="text-center w-full">
                     <Image
                         src="/glob.webp"
