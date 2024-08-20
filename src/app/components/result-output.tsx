8559219372f1e5fd22544f7d95635fe447944e36
shahab <oveysi.shahab@gmail.com>
2024-08-21 00:21:27 +0330

    fix: Fixing decimal point problem

--- src/app/components/result-output.tsx
+++ src/app/components/result-output.tsx
@@ -10,7 +10,7 @@ const ResultOutput = (props: PropsType) => {
                 Your <b className="font-semibold">{props.label}</b> footprint is
             </h3>
             <p className="flex items-center gap-xs">
-                <span className="text-2xl font-semibold leading-12">{Math.round(props.amount, 2)}</span>
+                <span className="text-2xl font-semibold leading-12">{Math.round(props.amount * 100) / 100}</span>
                 <span className="text-md leading-8 font-extralight">
                     kgCO<sub>2</sub>e
                 </span>
